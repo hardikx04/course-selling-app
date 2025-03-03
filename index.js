31a547944298f8dacbbfa702d83a585161a33214
Hardik Kapoor <hardikkapoor72@gmail.com>
2025-03-03 23:56:39 +0530

    router updated

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const { courseRouter } = require("./routes/course");
 //Routing in express, the express Router
 const app = express();
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
-
-createUserRoutes(app);
-createCourseRoutes(app);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.listen(3000);
--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
 
-const courseRouter = "Router";
+const courseRouter = Router();
 
-courseRouter.post("/routes/course/purchase", function (req, res) {
+courseRouter.post("/purchase", function (req, res) {
   //you would expect the user to pay you money
   res.json({
     message: "signup endpoint",
   });
 });
 
-courseRouter.get("/routes/courses/preview", function (req, res) {
+courseRouter.get("/preview", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
